507685e2eff5cf9d9eef9a375a35b293de68f9af
Fanan-soufiane <115195347+Fanan-soufiane@users.noreply.github.com>
2022-10-08 20:50:41 +0100

    Add files via upload

---
+++ insta.py
@@ -0,0 +1,31 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;32m')
+    os.system ('clear')
+    print ("██╗░░██╗██╗░░██╗░░░░░██╗███╗░░██╗░██████╗████████╗░█████╗░")
+    print ("██║░░██║██║░██╔╝░░░░░██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗")
+    print ("███████║█████═╝░░░░░░██║██╔██╗██║╚█████╗░░░░██║░░░███████║")
+    print ("██╔══██║██╔═██╗░░░░░░██║██║╚████║░╚═══██╗░░░██║░░░██╔══██║")
+    print ("██║░░██║██║░╚██╗░░░░░██║██║░╚███║██████╔╝░░░██║░░░██║░░██║")
+    print ("╚═╝░░╚═╝╚═╝░░╚═╝░░░░░╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝")
+    time.sleep(0.2)
+    print ('\033[1;31m     دﺤاو بﺎﺴﺤ قاﺮﺘﺧإ كﻧﮑﻤﯾ VIP  تﺴﻟ تﻧأ')
+    fx9 = input (" \033[1;33m  ﺔﯾﺤﺿ بﺎﺴﺤ طﺑار  ==> \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == '____snexy_s' :
+            os.system ('python rot.py')
+            restart ()
+    elif fx9 == 'https://instagram.com/____snexy_s?igshid=YmMyMTA2M2Y=' :
+            os.system ('python rot.py')
+main (main)
+home_main ()
